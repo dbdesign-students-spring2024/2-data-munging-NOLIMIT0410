7971bde4813c6ca2c79bfe0d70736c2af03e650c
NOLIMIT0410 <ys4679@nyu.edu>
2024-02-12 22:36:05 -0500

    Final version

--- munge.py
+++ munge.py
@@ -6,9 +6,12 @@ csvwriter=csv.writer(out_file)
 f = open("data/GLB.Ts+dSST.txt", "r")
 print(f)
 isHeadFound=False
+#filter lines as we only want to work on lines that we need.
 for line in f:
+    #get rid of lines that are empty
     if not line.strip():
         continue
+    #keep only the first title
     if line.startswith("Year") and isHeadFound==True:
         continue
     if line.startswith("Year"):
@@ -16,10 +19,12 @@ for line in f:
     if not line.startswith("Year") and not line[0].isdigit():
         continue 
     line_split = line.split()
-    print (line_split)
+    #print (line_split)
+    #set all missing value as 0.0
     for idx in range(len(line_split)):
         if "*" in line_split[idx]:
             line_split[idx]="0.0"
+    #change unit of our values
     if not line_split[0]=="Year":
         for idx in range(1,len(line_split)-1):
             line_split[idx]=float(line_split[idx])
